95de66bdb3b9bf47fb7a9f2d53fffd3c18a6070f
Aris Ripandi <riipandi@gmail.com>
2022-11-05 07:55:57 +0700

    change meta config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  poweredByHeader: false,
+  output: 'standalone',
 }
 
 module.exports = nextConfig
--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,8 @@
 module.exports = {
+  jsxSingleQuote: true,
+  printWidth: 100,
   singleQuote: true,
+  useTabs: false,
   semi: false,
   plugins: [require('prettier-plugin-tailwindcss')],
 }
